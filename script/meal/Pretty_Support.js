1ff41e6d68fe849ae0cdaac750cb24aa868abac6
BackGwa <64456214+BackGwa@users.noreply.github.com>
2023-04-05 13:32:52 +0900

    자바스크립트 코드 이주 및 추가

---
+++ script/meal/Pretty_Support.js
@@ -0,0 +1,79 @@
+const randint = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let savemoji = '';
+let letmoji = '';
+
+const EmojiRandom = () => {
+    switch(randint(0, 10)) {
+        case 0: savemoji = '🍙'; break;
+        case 1: savemoji = '🍛'; break;
+        case 2: savemoji = '🍚'; break;
+        case 3: savemoji = '🍜'; break;
+        case 4: savemoji = '🍱'; break;
+        case 5: savemoji = '🍕'; break;
+        case 6: savemoji = '🥣'; break;
+        case 7: savemoji = '🍖'; break;
+        case 8: savemoji = '🥪'; break;
+        case 9: savemoji = '🥘'; break;
+        case 10: savemoji = '🍣'; break;
+    }
+    if(savemoji == letmoji){
+        letmoji = savemoji;
+        return EmojiRandom();
+    }else{
+        letmoji = savemoji;
+        return savemoji;
+    }
+}
+
+const MsgRandom = () => {
+    switch(randint(0, 7)) {
+        case 0: return '열심히 급식표를<br>읽고있는 중'
+        case 1: return '오늘 급식을<br>수소문 하는 중'
+        case 2: return '급식 아주머니에게<br>물어보는 중'
+        case 3: return '토스말고<br>이걸로 보는 중'
+        case 4: return '다른 반 급식표<br>빼돌리는 중'
+        case 5: return '데이터베이스<br>뜯어보는 중'
+        case 6: return '조과장에게<br>여쭤보는 중'
+        case 7: return '영준이가 셔틀<br>뛰는 중'
+    }
+}
+
+const PageRefrash = (emoji, message, iscreate) => {
+    const Class_Emoji = document.getElementById(emoji);
+    const Class_Message = document.getElementById(message);
+    if(iscreate){
+        Class_Emoji.innerHTML = EmojiRandom();
+        let msg = MsgRandom();
+        let counter = 0;
+        let dot = '';
+        Class_Message.innerHTML = msg;
+        setInterval(
+            function(){
+                Class_Emoji.classList.add('playanimeted_logo');
+                Class_Emoji.innerHTML = EmojiRandom();
+                counter++;
+                switch(counter){
+                    case 1: dot = '.'; break;
+                    case 2: dot = '..'; break;
+                    case 3: dot = '...'; break;
+                    case 4: dot = ''; counter = 0; break;
+                }
+                Class_Message.innerHTML = msg + dot;
+
+                setTimeout(function(){
+                    Class_Emoji.classList.remove('playanimeted_logo');
+                }, 250);
+            }
+        , 750);
+    }else{
+        const loadui = document.querySelector('.loadui');
+        loadui.classList.add('playanimated_sizeup');
+        sleep(400);
+        Class_Emoji.remove();
+        Class_Message.remove();
+    }
+    return 1
+}
---
+++ script/timetable/parameter.js
@@ -0,0 +1,46 @@
+function getParams(){
+    var url = window.location.search.replace('?','');
+    var params = {};
+    var urlArray = url.split('&');
+
+    for(var i in urlArray)
+    {
+      var param = urlArray[i].split('=');
+      params[param[0]] = param[1]; 
+    }
+    return params;
+}
+
+function ax(){
+  var pag = getParams();
+
+  var _Grade = pag["grade"];
+  var _Class = pag['class'];
+  const tm = document.getElementById('titlex');
+  tm.innerHTML = _Grade + '학년 ' + _Class + '반';
+
+  var datev = new Date();
+
+  datx = getDayOfWeek(`${datev.getFullYear()}-${datev.getMonth() + 1}-${datev.getDate()}`);
+
+  const indatx = document.getElementById('times' + datx);
+  indatx.classList.add('toptime');
+
+  var step;
+
+  for (step = 1; step <= 7; step++) {
+    let indabx = document.getElementById(`t${step}${datx}`);
+    indabx.classList.add('toptimebgx');
+  }
+
+}
+
+function getDayOfWeek(dstr){
+
+  const week = ['0', '1', '2', '3', '4', '5', '6'];
+
+  const dayOfWeek = week[new Date(dstr).getDay()];
+
+  return dayOfWeek;
+
+}
